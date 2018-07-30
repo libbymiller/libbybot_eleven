23abea2e738558850a6e92985dde26bdddba9721
libbymiller <libbymiller@users.noreply.github.com>
2018-07-30 23:31:01 +0100

    Improved command and update to bot12.html

--- pi/start_chromium.sh
+++ pi/start_chromium.sh
@@ -3,7 +3,7 @@
 myrandom=$RANDOM
 echo $myrandom
 
-# sometimes it helps to remove everything
+# sometimes useful to kill the whole profile
 #rm -rf /home/pi/.config/chromium/
 
 # this is for bad crashes, which leave a lock handing round
@@ -15,4 +15,6 @@ sudo /home/pi/server.py &
 sleep 1
 
 # run the browser
-/usr/bin/chromium-browser --kiosk --use-fake-ui-for-media-stream --disable-infobars --disable-session-crashed-bubble --no-first-run --allow-running-insecure-content --allow-insecure-localhost https://yourserver:8443/libbybot/bot11.html#$myrandom &
+# It does need to be kiosk, else the audio may not get picked up
+/usr/bin/chromium-browser --kiosk --use-fake-ui-for-media-stream --disable-infobars --disable-session-crashed-bubble --no-first-run 
+--allow-running-insecure-content --allow-insecure-localhost https://yourserver:8443/libbybot/bot12.html#$myrandom &
